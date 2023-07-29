5e0d63779f3d9f1b839fb3b17717472c60916d84
zhenquansim <s.zhenquan98@gmail.com>
2023-07-29 21:24:54 +0800

    Added mongoDB connection

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+    "context"
     "fmt"
     "net/http"
+    "os"
+    "log"
 
+    "github.com/joho/godotenv"
+    
     "github.com/zhenquansim/chat-app/pkg/websocket"
+    "go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func loadGoDotEnvVariable() {
+
+    // load .env file
+    err := godotenv.Load(".env")
+  
+    if err != nil {
+      log.Fatalf("Error loading .env file")
+    }
+}
 
 func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
     fmt.Println("WebSocket Endpoint Hit")
@@ -33,6 +51,37 @@ func setupRoutes() {
 }
 
 func main() {
+    loadGoDotEnvVariable()
+
+    var uri = fmt.Sprintf("mongodb+srv://%s:%s@%s/?retryWrites=true&w=majority", 
+        os.Getenv("DATABASE_USERNAME"),
+        os.Getenv("DATABASE_PASSWORD"),
+        os.Getenv("DATABASE_HOST"))
+
+    // Use the SetServerAPIOptions() method to set the Stable API version to 1
+	serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+	opts := options.Client().ApplyURI(uri).SetServerAPIOptions(serverAPI)
+
+    // Create a new client and connect to the server
+    client, err := mongo.Connect(context.TODO(), opts)
+
+    if err != nil {
+        panic(err)
+    }
+    defer func() {
+        if err = client.Disconnect(context.TODO()); err != nil {
+            panic(err)
+        }
+    }()
+
+    // Send a ping to confirm a successful connection
+    var result bson.M
+    if err := client.Database("main").RunCommand(context.TODO(), bson.D{{"ping", 1}}).Decode(&result); err != nil {
+        panic(err)
+    }
+
+    fmt.Println("Pinged your deployment. You successfully connected to MongoDB!")
+
     fmt.Println("Distributed Chat App v0.01")
     setupRoutes()
     http.ListenAndServe(":8080", nil)
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -3,7 +3,8 @@ var socket = new WebSocket("ws://localhost:8080/ws");
 let connect = (cb) => {
 	console.log("connecting");
 
-	socket.onopen = () => {
+	socket.onopen = (data) => {
+		console.log("data", data);
 		console.log("Successfully Connected");
 	};
 
