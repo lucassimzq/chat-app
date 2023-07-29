da637b6701a3c01d65241bf7e68dab851cddb01c
zhenquansim <s.zhenquan98@gmail.com>
2023-07-29 11:56:16 +0800

    Added chat input component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { connect, sendMsg } from "./api";
 import Header from "./components/Header/Header";
 import ChatHistory from "./components/ChatHistory/ChatHistory";
+import ChatInput from "./components/ChatInput";
 class App extends Component {
 	constructor(props) {
 		super(props);
@@ -21,9 +22,11 @@ class App extends Component {
 		});
 	}
 
-	send() {
-		console.log("hello");
-		sendMsg("hello");
+	send(event) {
+		if (event.keyCode === 13) {
+			sendMsg(event.target.value);
+			event.target.value = "";
+		}
 	}
 
 	render() {
@@ -31,7 +34,7 @@ class App extends Component {
 			<div className="App">
 				<Header />
 				<ChatHistory chatHistory={this.state.chatHistory} />
-				<button onClick={this.send}>Hit</button>
+				<ChatInput send={this.send} />
 			</div>
 		);
 	}
